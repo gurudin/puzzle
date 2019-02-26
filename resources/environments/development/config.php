db9f24ff855932a82a1809e106e709f01a4fa968
gaoxiang <4008353@qq.com>
2019-02-26 19:05:22 +0800

    fixed migrate up

--- resources/environments/development/config.php
+++ resources/environments/development/config.php
@@ -1,7 +1,34 @@
 <?php
-/**
- * Development environment configuration files。
- */
+/*
+|--------------------------------------------------------------------------
+| Development environment configuration files.
+|--------------------------------------------------------------------------
+*/
 return [
     'debug' => true,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Database Connections
+    |--------------------------------------------------------------------------
+    |
+    | Here are all of the database connection configurations for development and use.
+    |
+    */
+    'database' => [
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => '127.0.0.1',
+            'port' => '3306',
+            'database' => 'puzzle',
+            'username' => 'root',
+            'password' => '',
+            'unix_socket' => '',
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
+    ],
 ];
--- resources/environments/production/config.php
+++ resources/environments/production/config.php
@@ -1,7 +1,34 @@
 <?php
-/**
- * Production environment configuration files。
- */
+/*
+|--------------------------------------------------------------------------
+| Production environment configuration files.
+|--------------------------------------------------------------------------
+*/
 return [
     'debug' => false,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Database Connections
+    |--------------------------------------------------------------------------
+    |
+    | Here are all of the database connection configurations for development and use.
+    |
+    */
+    'database' => [
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => '127.0.0.1',
+            'port' => '3306',
+            'database' => 'puzzle',
+            'username' => 'root',
+            'password' => '',
+            'unix_socket' => '',
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
+    ],
 ];
